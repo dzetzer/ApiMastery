eae5044c691a45d8997ec3f06020067946e482cd
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-11-09 09:07:13 -1100

    first commit, added back end

---
+++ back-end/ApiMastery/ApiMastery/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMastery
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=ApiMastery;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
